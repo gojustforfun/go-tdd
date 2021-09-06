5b495e86f93c16d3bc5db61b677a404ef9854dd4
longyue0521 <longyueli0521@gmail.com>
2021-09-06 20:40:10 +0800

    test(money): add TestMultiplication test
    添加TestMultiplication测试
    
    遗留问题：public fields, side-effects, integers for monetary amounts.

--- money/money_test.go
+++ money/money_test.go
@@ -1,1 +1,13 @@
 package money
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMultiplication(t *testing.T) {
+	five := NewDollar(5)
+	five.times(2)
+	assert.Equal(t, 10, five.amount)
+}
