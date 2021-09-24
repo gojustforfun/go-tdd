2733ff953bccf35db1df24c3c3f5a6ae95954672
longyue0521 <longyueli0521@gmail.com>
2021-09-24 17:47:07 +0800

    ch1: [refactor] remove unuse field, rename field's name

--- template/template.go
+++ template/template.go
@@ -3,22 +3,20 @@ package template
 import "strings"
 
 type Template struct {
-	variables     map[string]string
-	variableValue string
-	content       string
+	variables map[string]string
+	text      string
 }
 
-func NewTemplate(content string) *Template {
-	return &Template{content: content, variables: make(map[string]string)}
+func NewTemplate(text string) *Template {
+	return &Template{text: text, variables: make(map[string]string)}
 }
 
 func (t *Template) Set(name string, value string) {
-	t.variableValue = value
 	t.variables[name] = value
 }
 
 func (t *Template) Evaluate() string {
-	text := t.content
+	text := t.text
 	for name, value := range t.variables {
 		text = strings.ReplaceAll(text, "${"+name+"}", value)
 	}
