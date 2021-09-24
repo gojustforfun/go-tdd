a09661bbd2f63577ad391e91bcda1ae9768ae781
longyue0521 <longyueli0521@gmail.com>
2021-09-24 17:30:32 +0800

    ch1: [fake it] introduce variables field, initialize it in NewTemplate, store variable name and value in Set, still fake impl in Evaluate

--- template/template.go
+++ template/template.go
@@ -3,18 +3,24 @@ package template
 import "strings"
 
 type Template struct {
+	variables     map[string]string
 	variableValue string
 	content       string
 }
 
 func NewTemplate(content string) *Template {
-	return &Template{content: content}
+	return &Template{content: content, variables: make(map[string]string)}
 }
 
 func (t *Template) Set(name string, value string) {
 	t.variableValue = value
+	t.variables[name] = value
 }
 
 func (t *Template) Evaluate() string {
-	return strings.ReplaceAll(t.content, "${name}", t.variableValue)
+	text := t.content
+	for name, value := range t.variables {
+		text = strings.ReplaceAll(text, "${"+name+"}", value)
+	}
+	return text
 }
